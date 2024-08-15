515beb63ad7712ef669f4e1a406076680b65f54d
Ibrahim Ghali <ghaliibrahim60@gmail.com>
2024-08-15 17:35:31 +0100

    enter to each laptop product for exctracting the data plus handling the pagination

--- spacenets/spiders/spacespider.py
+++ spacenets/spiders/spacespider.py
@@ -7,4 +7,14 @@ class SpacespiderSpider(scrapy.Spider):
     start_urls = ['https://spacenet.tn/74-pc-portable-tunisie']
 
     def parse(self, response):
-        pass
+        laptops = response.css('div.item.col-xs-6.col-sm-4.col-md-3.col-lg-3')
+        for laptop in laptops:
+            absolute_url = laptops.css('h2.product_name a::attr(href)').get()
+            yield response.follow(absolute_url, callback=self.parse_laptop_page)
+        
+        next_page = response.css('li a.js-search-link::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+    
+
