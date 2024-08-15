df26d0c2e55aefcfffd75952d9531437e08ae959
Ibrahim Ghali <ghaliibrahim60@gmail.com>
2024-08-15 16:14:13 +0100

    correct the url

--- spacenets/spiders/spacespider.py
+++ spacenets/spiders/spacespider.py
@@ -4,7 +4,7 @@ import scrapy
 class SpacespiderSpider(scrapy.Spider):
     name = 'spacespider'
     allowed_domains = ['https://spacenet.tn/']
-    start_urls = ['http://https://spacenet.tn//']
+    start_urls = ['https://spacenet.tn/74-pc-portable-tunisie']
 
     def parse(self, response):
         pass
