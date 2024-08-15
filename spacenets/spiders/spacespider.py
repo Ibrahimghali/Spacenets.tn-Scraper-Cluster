d1e33606f3e08949bb97ee8ec5c24f3d1505d8b3
Ibrahim Ghali <ghaliibrahim60@gmail.com>
2024-08-15 17:40:20 +0100

    exctracting the name with price

--- spacenets/spiders/spacespider.py
+++ spacenets/spiders/spacespider.py
@@ -3,18 +3,23 @@ import scrapy
 
 class SpacespiderSpider(scrapy.Spider):
     name = 'spacespider'
-    allowed_domains = ['https://spacenet.tn/']
+    allowed_domains = ['spacenet.tn']
     start_urls = ['https://spacenet.tn/74-pc-portable-tunisie']
 
     def parse(self, response):
         laptops = response.css('div.item.col-xs-6.col-sm-4.col-md-3.col-lg-3')
         for laptop in laptops:
-            absolute_url = laptops.css('h2.product_name a::attr(href)').get()
+            absolute_url = laptop.css('h2.product_name a::attr(href)').get()
             yield response.follow(absolute_url, callback=self.parse_laptop_page)
         
         next_page = response.css('li a.js-search-link::attr(href)').get()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
 
-    
-
+    def parse_laptop_page(self, response):
+        yield {
+            'name': response.css('h1::text').get().strip(),
+            'price': response.css('span.price::text').get().strip(),
+            #'description': response.css('div.description-content p::text').getall(),
+            #'availability': response.css('div.stock-status span::text').get().strip(),
+        }
