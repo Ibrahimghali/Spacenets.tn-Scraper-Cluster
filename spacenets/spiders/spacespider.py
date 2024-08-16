bba5b69e4aac3fe13b40fe39015e7416c826ee09
Ibrahim Ghali <ghaliibrahim60@gmail.com>
2024-08-16 11:24:11 +0100

    Fixing error in progress: scraped only the first page

--- spacenets/spiders/spacespider.py
+++ spacenets/spiders/spacespider.py
@@ -1,6 +1,5 @@
 import scrapy
-from spacenets.items import SpacenetsItem  # replace 'your_project_name' with your actual project name
-
+from spacenets.items import SpacenetsItem  
 class SpacespiderSpider(scrapy.Spider):
     name = 'spacespider'
     allowed_domains = ['spacenet.tn']
@@ -19,26 +18,24 @@ class SpacespiderSpider(scrapy.Spider):
     def parse_laptop_page(self, response):
         item = SpacenetsItem()
         
-        item['name'] = response.css('h1::text').get().strip()
+        item['name'] = response.css('h1::text').get()
         item['price'] = response.css('div.current-price span::attr(content)').get()
-        item['formatted_price'] = response.css('div.current-price span::text').get().strip()
-        item['description'] = response.css('div.description-content p::text').getall()
-        item['availability'] = response.css('div.stock-status span::text').get().strip()
-        item['operating_system'] = response.css('dt.name:contains("Système d\'exploitation") + dd.value::text').get().strip()
-        item['memory'] = response.css('dt.name:contains("Mémoire") + dd.value::text').get().strip()
-        item['ports'] = response.css('dt.name:contains("Ports") + dd.value::text').get().strip()
-        item['wireless_connectivity'] = response.css('dt.name:contains("Connectivité sans-fil") + dd.value::text').get().strip()
-        item['warranty'] = response.css('dt.name:contains("Garantie") + dd.value::text').get().strip()
-        item['screen_size'] = response.css('dt.name:contains("Taille de l\'écran") + dd.value::text').get().strip()
-        item['processor_type'] = response.css('dt.name:contains("Type de Processeur") + dd.value::text').get().strip()
-        item['hard_drive'] = response.css('dt.name:contains("Disque Dur") + dd.value::text').get().strip()
-        item['cache'] = response.css('dt.name:contains("Cache") + dd.value::text').get().strip()
-        item['graphics_card'] = response.css('dt.name:contains("Carte Graphique") + dd.value::text').get().strip()
-        item['processor_details'] = response.css('dt.name:contains("processeur") + dd.value::text').get().strip()
-        item['color'] = response.css('dt.name:contains("Couleur") + dd.value::text').get().strip()
-        item['touchscreen'] = response.css('dt.name:contains("Tactile") + dd.value::text').get().strip()
-        item['gamer'] = response.css('dt.name:contains("Gamer") + dd.value::text').get().strip()
-        item['graphics_card_ref'] = response.css('dt.name:contains("Réf Carte Graphique") + dd.value::text').get().strip()
-        item['pc_range'] = response.css('dt.name:contains("Gamme PC") + dd.value::text').get().strip()
+        item['formatted_price'] = response.css('div.current-price span::text').get()
+        item['operating_system'] = response.css('dt.name:contains("Système d\'exploitation") + dd.value::text').get()
+        item['memory'] = response.css('dt.name:contains("Mémoire") + dd.value::text').get()
+        item['ports'] = response.css('dt.name:contains("Ports") + dd.value::text').get()
+        item['wireless_connectivity'] = response.css('dt.name:contains("Connectivité sans-fil") + dd.value::text').get()
+        item['warranty'] = response.css('dt.name:contains("Garantie") + dd.value::text').get()
+        item['screen_size'] = response.css('dt.name:contains("Taille de l\'écran") + dd.value::text').get()
+        item['processor_type'] = response.css('dt.name:contains("Type de Processeur") + dd.value::text').get()
+        item['hard_drive'] = response.css('dt.name:contains("Disque Dur") + dd.value::text').get()
+        item['cache'] = response.css('dt.name:contains("Cache") + dd.value::text').get()
+        item['graphics_card'] = response.css('dt.name:contains("Carte Graphique") + dd.value::text').get()
+        item['processor_details'] = response.css('dt.name:contains("processeur") + dd.value::text').get()
+        item['color'] = response.css('dt.name:contains("Couleur") + dd.value::text').get()
+        item['touchscreen'] = response.css('dt.name:contains("Tactile") + dd.value::text').get()
+        item['gamer'] = response.css('dt.name:contains("Gamer") + dd.value::text').get()
+        item['graphics_card_ref'] = response.css('dt.name:contains("Réf Carte Graphique") + dd.value::text').get()
+        item['pc_range'] = response.css('dt.name:contains("Gamme PC") + dd.value::text').get()
 
         yield item
