a638d2707ccbc81b762dfe4f0f3477f04b049049
Ibrahim Ghali <ghaliibrahim60@gmail.com>
2024-08-17 16:40:14 +0100

    organize the code

--- spacenets/spiders/laptop_spider.py
+++ spacenets/spiders/laptop_spider.py
@@ -1,73 +1,66 @@
 import scrapy
 from spacenets.items import LaptopItem
-
 from spacenets.spiders.common_spider import CommonSpider
 
 class LaptopSpider(CommonSpider):
+    # Name of the spider, used to run the spider with Scrapy commands
     name = 'laptop_spider'
+
+    # Starting URL for the spider to begin scraping
     start_urls = ['https://spacenet.tn/18-ordinateur-portable']
 
     def parse(self, response):
+        """
+        The main parsing method that Scrapy calls with the response from the start URL.
+        This method extracts the links to individual laptop pages and yields them for further parsing.
+        """
+        # Select all laptop items on the page
         laptops = response.css('div.item.col-xs-6.col-sm-4.col-md-3.col-lg-3')
+        
+        # Iterate over each laptop item and extract the URL
         for laptop in laptops:
             url = laptop.css('h2.product_name a::attr(href)').get()
             if url:
+                # Follow the laptop URL and call 'parse_laptop_page' to scrape details
                 yield response.follow(url, callback=self.parse_laptop_page)
-        # Handle pagination
+        
+        # Handle pagination to move to the next page, if available
         yield from self.handle_pagination(response, self.parse)
-
-
-
-    # def handle_pagination(self, response):
-    #     # Print debug information
-    #     print("Handling pagination...")
-
-    #     # Extract the pagination links
-    #     hrefs = response.css('li a.js-search-link::attr(href)').getall()
-    #     print("Pagination links extracted:", hrefs)
-
-    #     if hrefs is not None:
-    #         # Access the last element of the pagination links
-    #         last_href = hrefs[-1]
-    #         if last_href:
-    #             # Follow the link to the next page
-    #             yield response.follow(response.urljoin(last_href), callback=self.parse)
-    #         else:
-    #             print("No last href found")
-    #     else:
-    #         print("No pagination links found")
     
-
     def parse_laptop_page(self, response):
+        """
+        Method to parse the details of an individual laptop.
+        It extracts various attributes of the laptop and stores them in a LaptopItem.
+        """
         # Create an instance of LaptopItem to store the scraped data
         item = LaptopItem()
         
-        # Extract various attributes of the laptop from the page using CSS selectors and store them in the item
+        # Extract and store the laptop's name
         item['name'] = response.css('h1::text').get()
+        
+        # Extract and store the laptop's price and formatted price
         item['price'] = response.css('div.current-price span::attr(content)').get()
         item['formatted_price'] = response.css('div.current-price span::text').get()
+
+        # Extract and store the laptop's warranty information
         item['Garranty'] = response.css('dt.name:contains("Garantie") + dd.value::text').get()
 
+        # Extract and store various specifications of the laptop
         item['operating_system'] = response.css('dt.name:contains("Système d\'exploitation") + dd.value::text').get()
         item['memory'] = response.css('dt.name:contains("Mémoire") + dd.value::text').get()
         item['ports'] = response.css('dt.name:contains("Ports") + dd.value::text').get()
         item['wireless_connectivity'] = response.css('dt.name:contains("Connectivité sans-fil") + dd.value::text').get()
-        
         item['screen_size'] = response.css('dt.name:contains("Taille de l\'écran") + dd.value::text').get()
         item['processor_type'] = response.css('dt.name:contains("Type de Processeur") + dd.value::text').get()
         item['hard_drive'] = response.css('dt.name:contains("Disque Dur") + dd.value::text').get()
         item['cache'] = response.css('dt.name:contains("Cache") + dd.value::text').get()
-
         item['graphics_card'] = response.css('dt.name:contains("Carte Graphique") + dd.value::text').get()
         item['processor_details'] = response.css('dt.name:contains("processeur") + dd.value::text').get()
         item['color'] = response.css('dt.name:contains("Couleur") + dd.value::text').get()
         item['touchscreen'] = response.css('dt.name:contains("Tactile") + dd.value::text').get()
-        
         item['gamer'] = response.css('dt.name:contains("Gamer") + dd.value::text').get()
         item['graphics_card_ref'] = response.css('dt.name:contains("Réf Carte Graphique") + dd.value::text').get()
         item['pc_range'] = response.css('dt.name:contains("Gamme PC") + dd.value::text').get()
 
-        # Yield the item to the pipeline for further processing
+        # Yield the item to the pipeline for further processing or storage
         yield item
-
-    
