e1cd9cad0a898b3be7d74b79ef11247388ee798b
Ibrahim Ghali <ghaliibrahim60@gmail.com>
2024-08-17 13:37:08 +0100

    implement the inheritence for the two classes

--- spacenets/items.py
+++ spacenets/items.py
@@ -1,41 +1,36 @@
 import scrapy
 
-class LaptopItem(scrapy.Item):
-    # Define fields specific to laptops
+class ProductItem(scrapy.Item):
+    # Define fields common to both laptops and computers
     name = scrapy.Field()
     price = scrapy.Field()
     formatted_price = scrapy.Field()
+    warranty = scrapy.Field()
+    screen_size = scrapy.Field()
+    color = scrapy.Field()
+    touchscreen = scrapy.Field()
+    gamer = scrapy.Field()
+
+class LaptopItem(ProductItem):
+    # Define fields specific to laptops
     operating_system = scrapy.Field()
     memory = scrapy.Field()
     ports = scrapy.Field()
     wireless_connectivity = scrapy.Field()
-    warranty = scrapy.Field()
-    screen_size = scrapy.Field()
     processor_type = scrapy.Field()
     hard_drive = scrapy.Field()
     cache = scrapy.Field()
     graphics_card = scrapy.Field()
     processor_details = scrapy.Field()
-    color = scrapy.Field()
-    touchscreen = scrapy.Field()
-    gamer = scrapy.Field()
     graphics_card_ref = scrapy.Field()
     pc_range = scrapy.Field()
 
-class ComputerItem(scrapy.Item):
+class ComputerItem(ProductItem):
     # Define fields specific to computers
-    name = scrapy.Field()
-    price = scrapy.Field()
-    formatted_price = scrapy.Field()
-    warranty = scrapy.Field()
-    screen_size = scrapy.Field()
     brightness = scrapy.Field()
     contrast_ratio = scrapy.Field()
     response_time = scrapy.Field()
     screen_format = scrapy.Field()
     connectors = scrapy.Field()
-    color = scrapy.Field()
     panel_type = scrapy.Field()
-    touchscreen = scrapy.Field()
-    gamer = scrapy.Field()
     refresh_rate = scrapy.Field()
