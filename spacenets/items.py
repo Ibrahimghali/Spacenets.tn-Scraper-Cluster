6a2ffc99fdd35b10975c7b6364b259ebe95bbf2b
Ibrahim Ghali <ghaliibrahim60@gmail.com>
2024-08-18 11:52:48 +0100

    Update the parse_laptop_page method to use a loop for more flexible structure for different data type

--- spacenets/items.py
+++ spacenets/items.py
@@ -1,48 +1,10 @@
 import scrapy
 
-class ProductItem(scrapy.Item):
-    # Define fields common to both laptops and computers
+class ProductFeaturesItem(scrapy.Item):
+    # Fields for additional product details
     name = scrapy.Field()
     price = scrapy.Field()
     formatted_price = scrapy.Field()
-    Garranty = scrapy.Field()
     
-    
-
-class LaptopItem(ProductItem):
-    # Define fields specific to laptops
-    operating_system = scrapy.Field()
-    memory = scrapy.Field()
-    ports = scrapy.Field()
-    wireless_connectivity = scrapy.Field()
-
-    screen_size = scrapy.Field()
-    processor_type = scrapy.Field()
-    hard_drive = scrapy.Field()
-
-    cache = scrapy.Field()
-    graphics_card = scrapy.Field()
-    processor_details = scrapy.Field()
-    
-    color = scrapy.Field()
-    touchscreen = scrapy.Field()
-    gamer = scrapy.Field()
-    
-    graphics_card_ref = scrapy.Field()
-    pc_range = scrapy.Field()
-
-
-
-class AirConditionerItem(ProductItem):
-    
-    couleur = scrapy.Field()
-    mode = scrapy.Field()
-    capacite = scrapy.Field()
-    inverter = scrapy.Field()
-    type_climatiseur = scrapy.Field()
-    smart = scrapy.Field()
-    classe_temperatures = scrapy.Field()
-
-
-
-
+    # Field for product features
+    features = scrapy.Field()
--- spacenets/spiders/laptop_spider.py
+++ spacenets/spiders/laptop_spider.py
@@ -1,5 +1,6 @@
 import scrapy
-from spacenets.items import LaptopItem
+#from spacenets.items import LaptopItem
+from spacenets.items import ProductFeaturesItem
 from spacenets.spiders.common_spider import CommonSpider
 
 class LaptopSpider(CommonSpider):
@@ -7,31 +8,30 @@ class LaptopSpider(CommonSpider):
     start_urls = ['https://spacenet.tn/18-ordinateur-portable']
 
     def parse_laptop_page(self, response):
+
         """
         Method to parse detailed information from a laptop page.
         This method is specific to the LaptopSpider and handles fields relevant to laptops.
         """
-        item = LaptopItem()
+        # Create an instance of the Item
+        item = ProductFeaturesItem()
+
+        # Extract and assign the additional fields
         item['name'] = response.css('h1::text').get()
         item['price'] = response.css('div.current-price span::attr(content)').get()
         item['formatted_price'] = response.css('div.current-price span::text').get()
-        item['Garranty'] = response.css('dt.name:contains("Garantie") + dd.value::text').get()
-        item['operating_system'] = response.css('dt.name:contains("Système d\'exploitation") + dd.value::text').get()
 
-        item['memory'] = response.css('dt.name:contains("Mémoire") + dd.value::text').get()
-        item['ports'] = response.css('dt.name:contains("Ports") + dd.value::text').get()
-        item['wireless_connectivity'] = response.css('dt.name:contains("Connectivité sans-fil") + dd.value::text').get()
-        item['screen_size'] = response.css('dt.name:contains("Taille de l\'écran") + dd.value::text').get()
-        item['processor_type'] = response.css('dt.name:contains("Type de Processeur") + dd.value::text').get()
-        item['hard_drive'] = response.css('dt.name:contains("Disque Dur") + dd.value::text').get()
-        item['cache'] = response.css('dt.name:contains("Cache") + dd.value::text').get()
-        item['graphics_card'] = response.css('dt.name:contains("Carte Graphique") + dd.value::text').get()
-        item['processor_details'] = response.css('dt.name:contains("processeur") + dd.value::text').get()
-        item['color'] = response.css('dt.name:contains("Couleur") + dd.value::text').get()
-        item['touchscreen'] = response.css('dt.name:contains("Tactile") + dd.value::text').get()
-        item['gamer'] = response.css('dt.name:contains("Gamer") + dd.value::text').get()
-        item['graphics_card_ref'] = response.css('dt.name:contains("Réf Carte Graphique") + dd.value::text').get()
-        item['pc_range'] = response.css('dt.name:contains("Gamme PC") + dd.value::text').get()
+        # Extract the keys (names of the features)
+        keys = response.css('section.product-features dl.data-sheet dt.name::text').getall()
 
-        # Yield the item to the pipeline for further processing
+        # Extract the values (values of the features)
+        values = response.css('section.product-features dl.data-sheet dd.value::text').getall()
+
+        # Combine keys and values into a dictionary
+        product_features = {key.strip(): value.strip() for key, value in zip(keys, values)}
+
+        # Assign the dictionary to the 'features' field of the item
+        item['features'] = product_features
+
+        # Yield the item
         yield item
